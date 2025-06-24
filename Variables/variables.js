ac1767c59cad62b657db2a1ea4c2433782bf062d
ErioedMilano <erioedjr@hotmail.com>
2025-06-23 23:33:54 -0300

    Les 1 variables

--- Variables/variables.js
+++ Variables/variables.js
@@ -1,1 +1,9 @@
 console.log('Happy developing ✨')
+
+let fullName = "Dark Vader";
+let age = 25;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `you are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
