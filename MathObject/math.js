ad287318a61150bc6c185bb2d6cd2b041c244766
ErioedMilano <erioedjr@hotmail.com>
2025-06-28 23:47:56 -0300

    Les 4 final Math Object

---
+++ MathObject/math.js
@@ -0,0 +1,33 @@
+const pi = Math.PI
+
+console.log(pi)
+
+const x = 5.55;
+const y = 3.33;
+const num1 = 2;
+const num2 = 3;
+
+let z;
+let a;
+let b;
+let c;
+let e;
+
+
+z = Math.round(x);
+a = Math.floor(x);
+b = Math.ceil(y);
+c = Math.trunc(y);
+e = Math.pow(num1, num2);
+
+let max = Math.max(num1,num2,x,y)
+let min = Math.min(num1,num2,x,y)
+
+
+console.log(z);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(e);
+console.log("Maximum is : " + max)
+console.log(`minimum is : ${min}`)
