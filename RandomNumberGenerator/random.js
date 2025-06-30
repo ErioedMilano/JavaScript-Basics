00f6c68ea8d6430c1f5ee86504d9cceb826d9832
ErioedMilano <erioedjr@hotmail.com>
2025-06-29 23:58:41 -0300

    Les 5 update Random number generator

--- RandomNumberGenerator/random.js
+++ RandomNumberGenerator/random.js
@@ -8,3 +8,15 @@ let randomNum2 = Math.floor(Math.random() * (max - min)) + min;
 
 console.log(randomNum1);
 console.log(randomNum2);
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+
+const minimum = 1;
+const maximum = 6;
+let randomNum;
+
+myButton.onclick = function (){
+    randomNum = Math.floor(Math.random() * maximum) + minimum;
+    myLabel.textContent = randomNum;
+}
