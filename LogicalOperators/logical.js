ced7304a649109b9506f3d7e9d93a009f211c3ba
ErioedMilano <erioedjr@hotmail.com>
2025-07-07 23:47:47 -0300

    Les 11 LOGICAL OPERATORS

---
+++ LogicalOperators/logical.js
@@ -0,0 +1,23 @@
+// LOGICAL OPERATORS
+
+const temp = 0;
+
+// AND = &&
+
+if (temp > 0 && temp <= 30){
+    console.log("The weather is good.");
+}
+else {
+    console.log("The weather is bad.");
+}
+
+// NOT = !
+
+const isSunny = false;
+
+if (!isSunny){
+    console.log("It is cloudy")
+}
+else {
+    console.log("It is sunny")
+}
