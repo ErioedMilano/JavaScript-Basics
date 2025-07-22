0b6860d541dbcbed1949d478d32421bfea669378
ErioedMilano <erioedjr@hotmail.com>
2025-07-22 02:36:51 -0300

    Les 22 Random Password Generator final

--- RandomPasswordGenerator/password.js
+++ RandomPasswordGenerator/password.js
@@ -2,7 +2,51 @@
 
 function generatePassword(length,includeLowerCase,includeUpperCase,includeNumbers,includeSymbols){
 
-    return "";
+    const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolsChars = "!@#$%&()*_-+=";
+
+    let allowedChars = "";
+    let password = "";
+
+    if (includeLowerCase){
+        allowedChars += lowerCaseChars;
+    }
+    else {
+        allowedChars += "";
+    }
+    if (includeUpperCase){
+        allowedChars += upperCaseChars;
+    }
+    else {
+        allowedChars += "";
+    }
+    if (includeNumbers){
+        allowedChars += numberChars;
+    }
+    else {
+        allowedChars += "";
+    }
+    if (includeSymbols){
+        allowedChars += symbolsChars;
+    }
+    else {
+        allowedChars += "";
+    }
+
+    if (length <= 0){
+        return "Password length must be at least 1";
+    }
+    if (allowedChars.length === 0){
+        return "At least one set of characters needs to be selected";
+    }
+
+    for (let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+    return password;
 }
 
 const passwordLength = 12;
