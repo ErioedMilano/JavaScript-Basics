455a590e4886780303bf8185da071d881375a3aa
ErioedMilano <erioedjr@hotmail.com>
2025-08-16 23:24:35 -0300

    Destructuring

---
+++ Destructuring/destructuring.js
@@ -0,0 +1,26 @@
+// Destructuring is:
+// een manier om waarden uit arrays of objecten makkelijk in losse variabelen te halen,
+// zonder steeds punt- of indexnotatie te gebruiken.
+
+// ---------- EXAMPLE 1 ----------
+// SWAP THE VALUE OF TWO VARIABLES
+
+let a = 1;
+let b = 2;
+
+let fruit = "Dog";
+let animal = "apple";
+
+const nums = [a,b] = [b,a]
+const strings = [fruit,animal] = [animal,fruit]
+
+console.log(a);
+console.log(b);
+
+console.log(fruit);
+console.log(animal);
+
+
+// Array Destructuring
+
+
