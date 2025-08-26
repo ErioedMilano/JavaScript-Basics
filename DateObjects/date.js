f46e62fb33567c615aebefbdba41b33ae3cd7357
ErioedMilano <erioedjr@hotmail.com>
2025-08-26 11:11:00 -0300

    Les 43 Date Objects

---
+++ DateObjects/date.js
@@ -0,0 +1,14 @@
+// Date(year, month, day, hour, minute, second, ms)
+
+const date = new Date;
+console.log(date);
+
+const year = date.getFullYear();
+console.log(year);
+
+const month = date.getMonth();
+console.log(month);
+
+const day = date.getDate();
+console.log(day);
+
