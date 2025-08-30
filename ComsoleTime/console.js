0328423fc693a56128f397440f59e0dec5228477
ErioedMilano <erioedjr@hotmail.com>
2025-08-29 23:38:55 -0300

    Les 46 Console.time

---
+++ ComsoleTime/console.js
@@ -0,0 +1,36 @@
+// console.time() = tool that allows you to measure the time it takes
+//                              for a section of code or process to execute
+//                              Great for identifying performance "bottlenecks"
+
+console.time("test");
+
+for (let i = 0; i < 1000000; i++){
+    console.log(`number: ${i}`)
+}
+
+console.timeEnd("test");
+
+function loadData(){
+
+    console.time("loadData");
+
+    for(let i = 0; i < 1000000000; i++){
+        //pretend to load some data
+    }
+
+    console.timeEnd("loadData");
+}
+
+function processData(){
+
+    console.time("processData");
+
+    for(let i = 0; i < 1000000; i++){
+        //pretend to process some data
+    }
+
+    console.timeEnd("processData");
+}
+
+loadData();
+processData();
