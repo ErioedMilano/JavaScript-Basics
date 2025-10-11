e4caacbeb66829f7b7867b893b62c2991e12145e
ErioedMilano <erioedjr@hotmail.com>
2025-10-11 14:11:26 -0300

    Les 63 weather app javascript

---
+++ WeatherApp/weather.js
@@ -0,0 +1,26 @@
+// weather app
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "66cd743fc053a86433924ff6ca171fb5";
+
+weatherForm.addEventListener("submit", event =>{
+
+});
+
+async function getWeatherData(city){
+
+}
+
+function displayWeatherInfo(data){
+
+}
+
+function getWeatherImoj(weatherId){
+
+}
+
+function displayError(message){
+
+}
