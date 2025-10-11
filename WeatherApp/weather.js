6a7187a25fc30e19b33fbe1ca51a50979bd99765
ErioedMilano <erioedjr@hotmail.com>
2025-10-11 16:08:19 -0300

    Les 63 EventLisner part 2 & getWeatherData function

--- WeatherApp/weather.js
+++ WeatherApp/weather.js
@@ -5,14 +5,21 @@ const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "66cd743fc053a86433924ff6ca171fb5";
 
-weatherForm.addEventListener("submit", event =>{
+weatherForm.addEventListener("submit", async event =>{
 
     event.preventDefault();
 
     const city = cityInput.value;
 
     if (city){
-
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch (error){
+            console.error(error);
+            displayError(error);
+        }
     }
     else {
         displayError("Please Enter a city");
@@ -21,6 +28,15 @@ weatherForm.addEventListener("submit", event =>{
 
 async function getWeatherData(city){
 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
+
+    if (!response.ok){
+        throw new Error("Could not fetch weather data");
+    }
+    else {
+        return await response.json();
+    }
 }
 
 function displayWeatherInfo(data){
